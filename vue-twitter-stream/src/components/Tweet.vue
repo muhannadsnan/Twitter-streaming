015667a15be13124e22de271caa3e6f36044cb2f
muhannadsnan <msn-23@live.com>
2019-01-30 00:50:24 +0100

    format datetime

--- vue-twitter-stream/src/components/Tweet.vue
+++ vue-twitter-stream/src/components/Tweet.vue
@@ -2,7 +2,7 @@
   <div class="Tweet card mb-3 text-secondary px-3 pt-3 pb-0">
     <h5 class="username text-info">{{tweet.user.name}}:</h5>
     <p class="text-dark">{{tweet.text}}</p>
-    <p class="date text-right pt-2"><small>{{tweet.created_at}}</small></p>
+    <p class="date text-right pt-2"><small>{{datetime}}</small></p>
   </div>
 </template>
 
@@ -12,6 +12,24 @@ export default {
   props: ['tweet'],
   data(){
     return {
+    }
+  },
+  methods: {
+    formatDate(date) {
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+      var strTime = hours + ':' + minutes + ' ' + ampm;
+      return date.getDate() + ". " + date.getMonth()+1 + " " + date.getFullYear() + "  " + strTime;
+    },
+  },
+  computed: {
+    datetime(){
+      var date = new Date(this.tweet.created_at)
+      return this.formatDate(date)
     }
   },
   mounted(){
